4fac1929d26ea4126d6286e7a94febbbf0ef2d21
sbahibtihal-spec <sbahibtihal@gmail.com>
2026-08-04 20:32:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -128,3 +128,41 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
 });
 
 console.log("Portfolio Loaded Successfully");
+const words = [
+    "Front-End Developer",
+    "HTML",
+    "CSS",
+    "JavaScript"
+];
+
+let i = 0;
+let j = 0;
+let current = "";
+let isDeleting = false;
+
+function type() {
+    current = words[i];
+
+    if (!isDeleting) {
+        document.getElementById("typing").textContent =
+            current.substring(0, j++);
+    } else {
+        document.getElementById("typing").textContent =
+            current.substring(0, j--);
+    }
+
+    if (j === current.length + 1) {
+        isDeleting = true;
+        setTimeout(type, 1000);
+        return;
+    }
+
+    if (j === 0) {
+        isDeleting = false;
+        i = (i + 1) % words.length;
+    }
+
+    setTimeout(type, isDeleting ? 70 : 120);
+}
+
+type();
