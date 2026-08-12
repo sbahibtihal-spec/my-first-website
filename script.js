8c6c5f13426afd5b2e572760bb010dfcb466b6f0
sbahibtihal-spec <sbahibtihal@gmail.com>
2026-08-12 12:35:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -480,7 +480,25 @@ if (menuToggle && navLinks) {
             menuToggle.textContent = "☰";
 
         });
+// ==========================================
+// 8. قائمة الهاتف ☰
+// ==========================================
 
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+if (menuToggle && navLinks) {
+
+    menuToggle.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+        menuToggle.classList.toggle("active");
     });
 
+    // إغلاق القائمة بعد الضغط على أحد الروابط
+    navLinks.querySelectorAll("a").forEach(link => {
+        link.addEventListener("click", () => {
+            navLinks.classList.remove("active");
+            menuToggle.classList.remove("active");
+        });
+    });
 }
